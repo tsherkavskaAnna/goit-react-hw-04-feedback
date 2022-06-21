9f7797367a18691d9e5c162e9dec28cc07fecbe0
Anna Tsherkavska <katotoka1985@gmail.com>
2022-06-21 22:06:10 +0200

    fix bugs

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,13 +1,11 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-const Notification = ({message}) => (
-    <span>{message}</span>
-)
-
-
+const Notification = ({ message }) => {
+  return <span>{message}</span>;
+};
 
 Notification.propTypes = {
-    message: PropTypes.string.isRequired,
-}
+  message: PropTypes.string.isRequired,
+};
 
-export default Notification
+export default Notification;
